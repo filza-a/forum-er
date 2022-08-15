61bde3dcf4b15a9004686cda229a830af490d7fa
Filza Ahmad <64220634+filza-a@users.noreply.github.com>
2022-08-15 20:44:05 +0500

    Add files via upload

---
+++ forumer-script.js
@@ -0,0 +1,12 @@
+function upload(){
+    //add the forum to forum list
+    //display the list below the add forum block
+    //save/update the list in the local storage
+
+    let topic = document.getElementById("topic");
+    let content = document.getElementById("content");
+
+    var outerBox = document.createElement('div');
+    
+
+}
